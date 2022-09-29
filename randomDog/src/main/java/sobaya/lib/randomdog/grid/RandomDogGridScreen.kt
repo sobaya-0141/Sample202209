1d1a3b72f026f7278798dd476b1baa069b3cdc21
sobaya-0141 <soba.ha.kenkou@gmail.com>
2022-09-30 07:23:33 +0900

    try clickable

--- randomDog/src/main/java/sobaya/lib/randomdog/grid/RandomDogGridScreen.kt
+++ randomDog/src/main/java/sobaya/lib/randomdog/grid/RandomDogGridScreen.kt
@@ -1,25 +1,35 @@
 package sobaya.lib.randomdog.grid
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
 import sobaya.app.data.dogApi.response.RandomDogResponse
 import sobaya.app.util.Result
+import sobaya.app.util.ifTrue
 
 @OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
@@ -60,18 +70,29 @@ private fun RandomDogGridScreen(
         }
         is Result.Success -> {
             LazyVerticalGrid(
-                columns = GridCells.Adaptive(minSize = 128.dp),
-                modifier = modifier.fillMaxSize()
+                columns = GridCells.Fixed(3),
+                modifier = modifier.fillMaxSize(),
+                contentPadding = PaddingValues(
+                    start = 12.dp,
+                    top = 16.dp,
+                    end = 12.dp,
+                    bottom = 16.dp
+                )
             ) {
-                items(state.data.message) { message ->
+                items(
+                    items = state.data.message
+                ) { message ->
                     AsyncImage(
                         model = message,
                         contentDescription = "dog",
-                        contentScale = ContentScale.FillWidth,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                onClickImage(message)
+                        Modifier
+                            .width(128.dp)
+                            .height(128.dp)
+                            .padding(4.dp)
+                            .ifTrue(false) {
+                                clickable {
+                                    onClickImage(message)
+                                }
                             }
                     )
                 }
