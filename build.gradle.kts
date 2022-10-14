832e6e8f6015edaf018774011664c4fec875f6d5
sobaya-0141 <soba.ha.kenkou@gmail.com>
2022-10-14 21:18:57 +0900

    add screenshot test

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.application").version("7.4.0-alpha10").apply(false)
     kotlin("multiplatform").version("1.7.10").apply(false)
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
-    id("com.android.library") version "7.4.0-alpha10" apply false
+    id("com.android.library") version "8.0.0-alpha03" apply false
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
     id("com.squareup.sqldelight") version "1.5.3" apply false
 }
