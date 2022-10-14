3e851be54ee588ac7b213de7fcd8ff1396dbbbb6
sobaya-0141 <soba.ha.kenkou@gmail.com>
2022-10-14 21:57:52 +0900

    fixed lint

--- randomDog/src/main/java/sobaya/lib/randomdog/RandomDogScreen.kt
+++ randomDog/src/main/java/sobaya/lib/randomdog/RandomDogScreen.kt
@@ -4,14 +4,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import sobaya.app.data.dogApi.response.RandomDogResponse
@@ -60,7 +56,7 @@ private fun RandomDogScreen(
 @Preview
 fun RandomDogScreenPreview() {
     RandomDogScreen(
-        state = remember { mutableStateOf(Result.Loading) },
+        state = Result.Loading,
         onClickReload = {}
     )
 }
